673a2c9a8995204381b51227ce33498e5012cf8e
Gabriel Marks <82659871+GabrielMarkes@users.noreply.github.com>
2023-07-06 11:37:19 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Header from "./Header"
+import Main from "./Main"
+import Range from "./Range"
+import Footer1 from "./Footer1"
+import Footer2 from "./Footer2"
+import "./App.css"
+export default function App(){
+  return(
+    <div>
+      <Header/>
+      <Main/>
+      <Range/>
+      <Footer1/>
+      <Footer2/>
+    </div>
+  )
+}
